8e9d10c01fe067c2e23eec86ce9310da67923fbb
5h6Sm <5h6Sm.github@gmail.com>
2022-06-28 15:48:56 +0900

    html, css, js addNow()

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -28,3 +28,45 @@ toggleMenu('nav-toggle', 'nav-list');
 
 // const say = () => console.log('hello world3');
 // say();
+const addNow = (homeCardId) => {
+    // html -> js
+    const homeCard = document.getElementById(homeCardId);
+    // 시간 -> 몇번째 식사가 선택되어야하는지 결정
+    // 조식 끝 : 8:00
+    // 중식 끝 : 13:00
+    // 석식 끝 : 17:50
+    const now = new Date();
+    let hour;
+    let minute;
+    
+    hour = 17;
+    minute = 51;
+    console.log(hour, minute);
+    index = 0;
+    let minutes = hour*60 + minute;
+
+    if(minutes >= 17*60+50){
+        index = 0;
+    }else if(minutes <= 13*60){
+        index = 2;
+    }else if(minutes <= 8*60){
+        index = 1;
+    }
+    // if(hour > 8 && hour <= 13){
+    //     if(minute >= 0 ){
+    //         index = 1;
+    //     }
+    // }
+    // if(hour >= 13 && hour <= 17){
+    //     if(hour != 17 || hour == 17 && minute <= 50){
+    //         index = 2;
+    //     }
+    // }
+    console.log(index);
+
+    // homeCard에서 index번째 card에 now 클래스 추가
+    let card = homeCard.getElementsByClassName('card')[index];
+    card.classList.add('now');
+
+}
+addNow('home-card');
